06796e60a3fba9dbb65e05e095a7b884957b79da
tylertran349 <tylertran349@gmail.com>
2023-04-05 17:43:32 -0700

    Add remove comment button

--- src/Post.js
+++ src/Post.js
@@ -7,10 +7,9 @@ export function Post() {
     const [post, setPost] = useState({}); // Store data of current blog post
     const [user, setUser] = useState({}); // Store user that made the current blog post
     const [comments, setComments] = useState([]); // Stores array of comment objects, each containing the comment's data
-    const [commentUsers, setCommentUsers] = useState({}); // Stores key-value pairs of user IDs (key) and username (value)
+    const [commentUsers, setCommentUsers] = useState({}); // Stores key-value pairs of user IDs (key) and username (value) for every comment
     const [newComment, setNewComment] = useState("");
     const [numComments, setNumComments] = useState(0);
-    const [postIsLikedBy, setPostIsLikedBy] = useState(false);
 
     useEffect(() => {
         async function fetchPost() {
@@ -27,30 +26,9 @@ export function Post() {
             const user = await response.json();
             setUser(user);
         }
-        if (post.user) {
-            fetchUser();
-        }
     }, [post]);
 
     useEffect(() => {
-        async function fetchComments() {
-            const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}/comments?$expand=user`); // Fetch list of comments for the post
-            const commentIds = await response.json();
-            const commentRequests = commentIds.map(commentId => fetch(`https://blog-production-10b2.up.railway.app/comments/${commentId}`)); // Create an array of HTTP requests to fetch comment data for each comment using its ID
-            const commentResponses = await Promise.all(commentRequests); // Store an array of all the responses from the HTTP requests to fetch comment data for each comment
-            const comments = await Promise.all(commentResponses.map(response => response.json())); // Store an array of comment objects
-            setNumComments(comments.length);
-            setComments(comments); // Set the comments state to be the array of comment objects
-            
-            // Fetch usernames for all comments
-            const userIds = comments.map(comment => comment.user); // Create an array of all the user IDs for the entire list of comments
-            const userRequests = userIds.map(userId => fetch(`https://blog-production-10b2.up.railway.app/users/${userId}`)); // For each user ID, create an array of HTTP requests to fetch user data for each user using their ID
-            const userResponses = await Promise.all(userRequests); // Store an array of all the user API call responses
-            const users = await Promise.all(userResponses.map(response => response.json())); // Store an array of user objects
-            const userMap = {}; // Create a JavaScript object that maps user IDs to usernames
-            users.forEach(user => userMap[user._id] = user.username); // For each user object in the users array, create a new map entry that maps the user ID to its corresponding username
-            setCommentUsers(userMap);
-        }
         fetchComments();
     }, [postId, numComments]);
 
@@ -64,9 +42,29 @@ export function Post() {
         return formattedDate;
     }
 
+    async function fetchComments() {
+        const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}/comments?$expand=user`); // Fetch list of comments for the post
+        const commentIds = await response.json();
+        const commentRequests = commentIds.map(commentId => fetch(`https://blog-production-10b2.up.railway.app/comments/${commentId}`)); // Create an array of HTTP requests to fetch comment data for each comment using its ID
+        const commentResponses = await Promise.all(commentRequests); // Store an array of all the responses from the HTTP requests to fetch comment data for each comment
+        const comments = await Promise.all(commentResponses.map(response => response.json())); // Store an array of comment objects
+        setNumComments(comments.length);
+        setComments(comments); // Set the comments state to be the array of comment objects
+
+        console.log(comments);
+        // Fetch usernames for all comments
+        const userIds = comments.map(comment => comment.user); // Create an array of all the user IDs for the entire list of comments
+        const userRequests = userIds.map(userId => fetch(`https://blog-production-10b2.up.railway.app/users/${userId}`)); // For each user ID, create an array of HTTP requests to fetch user data for each user using their ID
+        const userResponses = await Promise.all(userRequests); // Store an array of all the user API call responses
+        const users = await Promise.all(userResponses.map(response => response.json())); // Store an array of user objects
+        const userMap = {}; // Create a JavaScript object that maps user IDs to usernames
+        users.forEach(user => userMap[user._id] = user.username); // For each user object in the users array, create a new map entry that maps the user ID to its corresponding username
+        setCommentUsers(userMap);
+    }
+
     async function handleCommentSubmit(event) {
         event.preventDefault(); // Prevents page from refreshing after new comment gets submitted
-        const response = await fetch("https://blog-production-10b2.up.railway.app/comments", {
+        const response = await fetch("https://blog-production-10b2.up.railway.app/comments/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -86,19 +84,13 @@ export function Post() {
     async function fetchPostIsLikedBy() {
         const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`);
         const postData = await response.json();
-        setPostIsLikedBy(postData.liked_by);
-        console.log(postData.liked_by);
     }
 
     async function likeButtonHandler() {
         const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`);
         const postData = await response.json();
-        console.log(postData);
-        console.log("Currently logged in user: " + getLoggedInUser());
         if(postData.liked_by.includes(getLoggedInUser())) { // If post is already liked by user
-            console.log("Post is already liked by user");
             let updatedLikedByList = postData.liked_by.filter(item => item !== getLoggedInUser());
-            setPostIsLikedBy(updatedLikedByList);
             await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`, {
                 method: "PUT",
                 headers: {
@@ -109,10 +101,9 @@ export function Post() {
                     liked_by: updatedLikedByList,
                 }),
             });
+            document.querySelector('#post-like-button').textContent = "Like Post";
         } else {
-            console.log("Post is not liked by user")
             let updatedLikedByList = postData.liked_by.concat(getLoggedInUser());
-            setPostIsLikedBy(updatedLikedByList);
             await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`, {
                 method: "PUT",
                 headers: {
@@ -123,6 +114,7 @@ export function Post() {
                     liked_by: updatedLikedByList,
                 }),
             });
+            document.querySelector('#post-like-button').textContent = "Liked";
         }
     }
 
@@ -142,13 +134,25 @@ export function Post() {
         }
     }
 
+    async function deleteCommentHandler(event) {
+        await fetch(`https://blog-production-10b2.up.railway.app/comments/${event.target.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+        fetchComments();
+    }
+
+    // TODO: Fix delete comment button and its handler
     if(localStorage.getItem("token") !== null) { // User is logged in
         return (
             <div>
                 <span>{post.title}</span>
                 <span>{post.content}</span>
                 <span>Posted by {user.username}</span>
-                <button type="button" onClick={likeButtonHandler}>Like Post</button>
+                <button id="post-like-button" type="button" onClick={likeButtonHandler}>Like Post</button>
                 <span>Comments</span>
                 <form onSubmit={handleCommentSubmit}>
                     <input id="comment-form" type="text" placeholder="Add a comment" value={newComment} onChange={(e) => setNewComment(e.target.value)}></input>
@@ -160,8 +164,9 @@ export function Post() {
                 {comments.map((comment) => {
                     return (
                     <div>
-                        <span>{comment.content}</span>
+                        {comment && (<span>{comment.content}</span>)}
                         <span>Posted by {commentUsers[comment.user]} on {formatDate(comment.date)}</span>
+                        {(comment.user === getLoggedInUser()) && (<button onClick={deleteCommentHandler} id={comment._id} type="button">Delete Comment</button>)}
                     </div>
                     )
                 })}
