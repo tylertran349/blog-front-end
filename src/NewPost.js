48437c3a3bf6687bce2f0089c39a8a1e12f19e52
tylertran349 <tylertran349@gmail.com>
2023-04-05 22:18:47 -0700

    Initial commit

---
+++ src/NewPost.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useHistory } from "react";
+import { useParams } from "react-router-dom";
+
+export function NewPost() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [published, setPublished] = useState(true);
+    async function handleSubmit(event) {
+        event.preventDefault(); // Prevent default form submission behavior
+        const response = await fetch("https://blog-production-10b2.up.railway.app/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
+            },
+            body: JSON.stringify({ 
+                title, 
+                content,
+                published: event.target.published.checked, // If checkbox is checked, it evaluates to true, otherwise, it's false
+            }),
+        });
+        if(response.ok) { // If response is within the range of 200-299 (successful request), then new blog post was successfully created and redirect to home page
+            window.location.href = "/";
+        } else {
+            window.location.href = "/new-post"; // If new blog post could not be created, redirect user back to the same page
+        }
+    }
+
+    return (
+        <div>
+            <span>Create a New Post</span>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Enter title</label>
+                <input id="title" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                <label htmlFor="content">Enter text</label>
+                <input id="content" type="text" placeholder="Text" value={content} onChange={(e) => setContent(e.target.value)}></input>
+                <label htmlFor="published">Do you want to make the post public?</label>
+                <input id="published" type="checkbox"></input>
+                <button type="submit">Create New Post</button>
+            </form>
+        </div>
+    );
+}
