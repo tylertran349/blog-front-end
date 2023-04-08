ab34fd00085b7af4bcd2174c1b00a38e79440f18
tylertran349 <tylertran349@gmail.com>
2023-04-07 21:54:26 -0700

    Add delete account button

--- src/Settings.js
+++ src/Settings.js
@@ -64,7 +64,21 @@ export function Settings() {
         }
     }
 
-    // TODO: Finish form, handler, and delete user button
+    async function deleteAccountHandler() {
+        const response = await fetch(`https://blog-production-10b2.up.railway.app/users/${userId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+        if(response.ok) { // If account was successfully deleted, redirect user to home page
+            window.location.href = "/logout";
+        } else {
+            window.location.href = `/users/${userId}/edit`; // If user could not be deleted, redirect user back to same page
+        }
+    }
+
     return (
         <div>
             <span>Change User Settings</span>
@@ -87,66 +101,7 @@ export function Settings() {
                 <input id="confirmNewPassword" type="password" placeholder="Re-enter new password" value={confirmNewPassword} onChange={(e) => setConfirmNewPassword(e.target.value)}></input>
                 <button type="submit">Save</button>
             </form>
+            <button onClick={deleteAccountHandler}>Delete Account</button>
         </div>
     );
 }
-
-/*
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
-export function EditPost() {
-    const { postId } = useParams(); // Extract post ID from URL parameter
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [published, setPublished] = useState(false);
-
-    useEffect(() => {
-        fetchPostData();
-    }, []); // Leave dependency array empty to only run the hook when the component mounts
-
-    async function fetchPostData() {
-        const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`);
-        const postData = await response.json();
-        setTitle(postData.title);
-        setContent(postData.content);
-        setPublished(postData.published);
-    }
-
-    async function handleSubmit(event) {
-        event.preventDefault(); // Prevent page from refreshing on form submission
-        const response = await fetch(`https://blog-production-10b2.up.railway.app/posts/${postId}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${localStorage.getItem("token")}`,
-            },
-            body: JSON.stringify({ 
-                title, 
-                content,
-                published,
-            }),
-        });
-        if(response.ok) { // If response is within the range of 200-299 (successful request), then redirect user back to the dedicated page for the post
-            window.location.href = `/posts/${postId}`;
-        } else {
-            window.location.href = `/posts/${postId}/edit`; // If blog post could not be updated, redirect user back to same page
-        }
-    }
-
-    return (
-        <div>
-            <span>Edit Post</span>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="title">Enter title</label>
-                <input id="title" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                <label htmlFor="content">Enter text</label>
-                <input id="content" type="text" placeholder="Text" value={content} onChange={(e) => setContent(e.target.value)}></input>
-                <label htmlFor="published">Do you want to make the post public?</label>
-                <input id="published" checked={published} type="checkbox" onChange={(e) => setPublished(e.target.checked)}></input>
-                <button type="submit">Save</button>
-            </form>
-        </div>
-    );
-}
-*/
