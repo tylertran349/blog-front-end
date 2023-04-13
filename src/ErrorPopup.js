d56dd430b2f7d2eabf56e7ba7ac7c5c50e5bf4e4
tylertran349 <tylertran349@gmail.com>
2023-04-12 22:39:07 -0700

    Add warning icon and hide error button

--- src/ErrorPopup.js
+++ src/ErrorPopup.js
@@ -3,7 +3,9 @@ import React from "react";
 export function ErrorPopup(props) {
     return (
         <div id="error-popup">
+            <span class="material-symbols-outlined" id="warning-icon">warning</span>
             <span id="error-message">{props.message}</span>
+            <button class="material-symbols-outlined" id="hide-error-button" onClick={props.onClick}>close</button>
         </div>
     );
 }
