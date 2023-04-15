482034374eceb1d008079f4722d3caf4381b8a47
tylertran349 <tylertran349@gmail.com>
2023-04-14 23:01:35 -0700

    Change HTML structure for styling

--- src/DeleteConfirmation.js
+++ src/DeleteConfirmation.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export function DeleteConfirmation(props) {
     return (
-        <div>
+        <div id="delete-confirmation-popup">
             {(props.type === "account") && (<span>Are you sure you want to delete your account?</span>)}
             {(props.type === "post") && (<span>Are you sure you want to delete this post?</span>)}
             {(props.type === "comment") && (<span>Are you sure you want to delete your comment?</span>)}
-            <button onClick={props.onConfirm}>Confirm</button>
-            <button onClick={props.onCancel}>Cancel</button>
+            <div id="buttons">
+                <button onClick={props.onConfirm} id="warning-button">Confirm</button>
+                <button onClick={props.onCancel}>Cancel</button>
+            </div>
         </div>
     );
 }
