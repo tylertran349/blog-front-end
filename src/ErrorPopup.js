9f2933aa59fe0df85f92b5e5bf0c8f7d45233de3
tylertran349 <tylertran349@gmail.com>
2023-04-14 23:01:52 -0700

    Change class attribute to className

--- src/ErrorPopup.js
+++ src/ErrorPopup.js
@@ -3,9 +3,9 @@ import React from "react";
 export function ErrorPopup(props) {
     return (
         <div id="error-popup">
-            <span class="material-symbols-outlined" id="warning-icon">warning</span>
+            <span className="material-symbols-outlined" id="warning-icon">warning</span>
             <span id="error-message">{props.message}</span>
-            <button class="material-symbols-outlined" id="hide-error-button" onClick={props.onClick}>close</button>
+            <button className="material-symbols-outlined" id="hide-error-button" onClick={props.onClick}>close</button>
         </div>
     );
 }
