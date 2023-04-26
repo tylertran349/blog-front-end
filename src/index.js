0d90c6d0cbcc51df39538a4039a8928a4815b863
tylertran349 <tylertran349@gmail.com>
2023-04-25 22:18:27 -0700

    Import and add Footer component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
+import { Footer } from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router />
+    <Footer />
   </React.StrictMode>
 );
