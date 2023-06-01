62293b5355a4fc3767d79e55235c7b83218c0a75
tylertran349 <tylertran349@gmail.com>
2023-05-31 18:02:51 -0700

    Rewrite to reduce API calls

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,34 +5,12 @@ import { NavBar } from "./NavBar";
 
 export function Homepage() {
     const [posts, setPosts] = useState([]); // An array that stores every post
-    const [usernames, setUsernames] = useState({}); // A JavaScript object that stores key-value pairs of user IDs (key) and their associated usernames (value)
     const [showErrorPopup, setShowErrorPopup] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
         fetchPosts(); // Get posts by making API call
     }, []); // Empty dependency array means this hook only runs one time when the component mounts (is inserted in the DOM tree)
-
-    useEffect(() => {
-        fetchUsernames();
-    }, [posts]); // This hook will run anytime the "posts" dependency changes
-
-    async function fetchUsernames() {
-        const userIds = posts.map(post => post.user); // Create an array named userIds with all the user IDs from each post object in the posts array
-        const usernameMap = {}; // JavaScript object (similar to hashmap) - userId is key, user.username is value
-        for(const userId of userIds) {
-            const response = await fetch(`https://blog-production-10b2.up.railway.app/users/${userId}`); // For each userId in the userIds array, fetch the user from the API
-            const user = await response.json();
-            if(!response.ok) {
-                setErrorMessage(user.error);
-                setShowErrorPopup(true);
-            } else {
-                setShowErrorPopup(false);
-            }
-            usernameMap[userId] = user.username; // Add new key-value pair to usernameMap object
-        }
-        setUsernames(usernameMap); // Update usernames state with updated usernameMap object
-    }
 
     async function fetchPosts() {
         const response = await fetch("https://blog-production-10b2.up.railway.app/posts");
@@ -44,6 +22,7 @@ export function Homepage() {
             setShowErrorPopup(false);
         }
         setPosts(data);
+        console.log(typeof data);
     }
 
     function formatDate(dateString) {
@@ -71,6 +50,8 @@ export function Homepage() {
         }
     }
 
+    console.log(posts);
+
     if(localStorage.getItem("token") !== null) { // User is logged in
         return (
             <div id="content">
@@ -79,12 +60,12 @@ export function Homepage() {
                 <a href={"/new-post"}><button id="new-post-button">Create a New Post</button></a>
                 {posts.length === 0 && (<span>There are no blog posts.</span>)}
                 {posts.slice().reverse().map((post) => {
-                    if(post.published || (getLoggedInUser() && getLoggedInUser()._id === post.user)) { // Also show all unpublished posts if logged in user is the author of the unpublished post(s)
+                    if(post.published || (getLoggedInUser() && (getLoggedInUser()._id === post.user._id))) { // Also show all unpublished posts if logged in user is the author of the unpublished post(s)
                         return (
                             <div id="post">
                                 <a href={`/posts/${post._id}`} id="title">{post.published ? post.title : `${post.title} (UNPUBLISHED)`}</a> {/* Add "(UNPUBLISHED)" at end of post title if post is unpublished */}
                                 <span id="post-content">{post.content}</span>
-                                <span>Posted by <a href={`/users/${post.user}`} id="user-link">{usernames[post.user]}</a> on {formatDate(post.date)}</span>
+                                <span>Posted by <a href={`/users/${post.user._id}`} id="user-link">{post.user.username}</a> on {formatDate(post.date)}</span>
                                 <div id="post-like-counter">
                                     <span className="material-symbols-outlined">thumb_up</span>
                                     <span>{post.liked_by.length}</span>
@@ -97,7 +78,7 @@ export function Homepage() {
                 })}
             </div>
         );    
-    } else { // User is not logged in
+    } else { // User is not logged in (only show published posts)
         return (
             <div id="content">
                 <NavBar loggedInUserId={getLoggedInUser() ? getLoggedInUser()._id : null} />
@@ -107,7 +88,7 @@ export function Homepage() {
                     <div id="post">
                         <a href={`/posts/${post._id}`} id="title">{post.title}</a>
                         <span id="post-content">{post.content}</span>
-                        <span>Posted by <a href={`/users/${post.user}`} id="user-link">{usernames[post.user]}</a> on {formatDate(post.date)}</span>
+                        <span>Posted by <a href={`/users/${post.user._id}`} id="user-link">{post.user.username}</a> on {formatDate(post.date)}</span>
                         <div id="post-like-counter">
                             <span className="material-symbols-outlined">thumb_up</span>
                             <span>{post.liked_by.length}</span>
