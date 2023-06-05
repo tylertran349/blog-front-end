eafe31e2847b5c1e94f018297e4ecae6eae29451
tylertran349 <tylertran349@gmail.com>
2023-06-05 16:05:01 -0700

    Change class attribute to className

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export function Footer() {
         <div id="footer">
             <div id="footer-contents">
                 <span>Copyright © 2023 Tyler Tran&ensp;</span>
-                <a href="https://github.com/tylertran349" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="https://github.com/tylertran349" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
             </div>
         </div>
     );
