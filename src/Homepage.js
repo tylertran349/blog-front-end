3a376eb8b15122953cb5128f27ffe8ab2cc17e67
tylertran349 <tylertran349@gmail.com>
2023-06-05 21:39:52 -0700

    Fix code for showing sort by menu

--- src/Homepage.js
+++ src/Homepage.js
@@ -177,21 +177,18 @@ export function Homepage() {
             {showErrorPopup && (<ErrorPopup message={errorMessage} onClick={(e) => setShowErrorPopup(false)} />)}
             {showDeletePostConfirmation && (<DeleteConfirmation type="post" onConfirm={deletePost} onCancel={() => setShowDeletePostConfirmation(false)} />)}
             <span id="title">Posts</span>
-            <div id="posts-actions">
-                <div id="dropdown">
-                    <button onClick={() => toggleDropdown()} id="dropbtn">Sort posts</button>
-                    <div id="dropdown-options" className="dropdown-content">
-                        <a onClick={() => setFilterPostsOption("Most recent")}>Most recent</a>
-                        <a onClick={() => setFilterPostsOption("Oldest")}>Oldest</a>
-                        <a onClick={() => setFilterPostsOption("Most liked")}>Most liked</a>
-                        <a onClick={() => setFilterPostsOption("Alphabetical")}>A-Z</a>
-                        <a onClick={() => setFilterPostsOption("Reverse alphabetical")}>Z-A</a>
-                    </div>
+            {(localStorage.getItem("token") !== null) && (<a href={"/new-post"}><button id="new-post-button">New post</button></a>)}
+            {((localStorage.getItem("token") !== null && (posts.filter(post => post.published || (post.user._id === getLoggedInUser()?._id)).length > 0)) || (localStorage.getItem("token") === null && posts.filter(post => post.published).length > 0)) && (<div id="dropdown">
+                <button onClick={() => toggleDropdown()} id="dropbtn">Sort posts</button>
+                <div id="dropdown-options" className="dropdown-content">
+                    <a onClick={() => setFilterPostsOption("Most recent")}>Most recent</a>
+                    <a onClick={() => setFilterPostsOption("Oldest")}>Oldest</a>
+                    <a onClick={() => setFilterPostsOption("Most liked")}>Most liked</a>
+                    <a onClick={() => setFilterPostsOption("Alphabetical")}>A-Z</a>
+                    <a onClick={() => setFilterPostsOption("Reverse alphabetical")}>Z-A</a>
                 </div>
-                {(localStorage.getItem("token") !== null) && (<a href={"/new-post"}><button id="new-post-button">New post</button></a>)}
-            </div>
-            {localStorage.getItem("token") !== null && posts.length === 0 && (<span>There are no blog posts.</span>)} {/* Only if the user is logged in and there are no published or unpublished posts at all */}
-            {localStorage.getItem("token") === null && posts.filter(post => post.published).length === 0 && (<span>There are no blog posts.</span>)} {/* Only if there are no published posts and the user is NOT logged in */}
+            </div>)}
+            {((localStorage.getItem("token") !== null && posts.filter(post => post.published || (post.user._id === getLoggedInUser()?._id)).length === 0) || (localStorage.getItem("token") === null && posts.filter(post => post.published).length === 0)) && (<span>There are no blog posts.</span>)} {/* Only if the user is logged in and there are no published or unpublished posts at all */}
             {filterPostsOption === "Most recent" && posts.slice().reverse().map((post) => { // Sort by most recent
                 if(post.published || (getLoggedInUser() && (getLoggedInUser()?._id === post.user._id))) { // Also show all unpublished posts if logged in user is the author of the unpublished post(s)
                     return (
